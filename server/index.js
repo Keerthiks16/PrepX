dd029641050e590bfa85597f6423b4e0d62f5ea3
Keerthik <keerthikshetty2@example.com>
2026-03-07 23:42:28 +0530

    pre deploy - server - 3

--- server/index.js
+++ server/index.js
@@ -16,9 +16,9 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173", // Frontend URL
+    origin: [process.env.CLIENT_URL, "http://localhost:5173", "http://localhost:3000"],
     credentials: true,
-  }),
+  })
 );
 app.use(express.json());
 app.use(cookieParser());
